93f334da2c182b0915b64308d45cf7917c4be126
ram kumar <rumk78864@gmail.com>
2023-10-05 13:11:11 +0530

    clear button added

--- src/components/Area.js
+++ src/components/Area.js
@@ -18,16 +18,22 @@ export default function Area(props) {
         let ltext = text.toLocaleLowerCase();
         setText(ltext);
     }
+
+    const cleartext = () =>{
+        setText("")
+    }
     return (
         <>  
             <div className="container my-3">
-            <h2>{props.title}</h2>
+            <h3>{props.title}</h3>
             <div className="mb-3">
-                <textarea className="form-control" id="myBox" value={text} onChange={change} rows="3"></textarea>
+                <textarea className="form-control" id="myBox" value={text} onChange={change} rows="6"></textarea>
 
             </div>
-            <button className="btn btn-primary mx-2" onClick={toUpCase}> Uppercase</button>
-            <button className="btn btn-primary mx-2" onClick={toLowCase}> Lowercase</button>
+            
+            <button className="btn btn-primary mx-2" onClick={toUpCase}>Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={toLowCase}>Lowercase</button>
+            <button className="btn btn-primary mx-2" onClick={cleartext}>Clear</button>
             </div>
 
             <div className="container my-3">
