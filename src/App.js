7ae5fc56a6884fe9095998842032341b850648e2
ram kumar <rumk78864@gmail.com>
2023-10-05 12:58:13 +0530

    added buttons and text area with conversion function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import NavBar from './components/NavBar';
-
+import Area from './components/Area';
 
 function App() {
   return (
-    <NavBar title="TextUtil" link2="About Us"/>
+    <>
+    <NavBar title="TextUtil" about="About Us"/>
+    <div className="container my-3">
+    <Area title="Enter Your Text Here"/>
+
+    </div>
+    </>
+    
   );
 }
 
---
+++ src/components/Area.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react'
+
+export default function Area(props) {
+
+    const [text,setText] = useState('Enter your Text');
+    // setText("new text");
+
+    const toUpCase = ()=>{
+        let newtext = text.toUpperCase();
+        setText(newtext)
+    }
+
+    const change = (event) =>{
+        setText(event.target.value)
+    }
+
+    const toLowCase = ()=>{
+        let ltext = text.toLocaleLowerCase();
+        setText(ltext);
+    }
+    return (
+        <>  
+            <div className="container my-3">
+            <h2>{props.title}</h2>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" value={text} onChange={change} rows="3"></textarea>
+
+            </div>
+            <button className="btn btn-primary mx-2" onClick={toUpCase}> Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={toLowCase}> Lowercase</button>
+            </div>
+
+            <div className="container my-3">
+
+                <h2>Text Summary</h2>
+                <p>{text.length} Characters</p>
+                <p>{text.split(" ").length} Words</p>
+
+                <h2>Preview</h2>
+                <p>{text}</p>
+
+                <p>Time to read : {0.008 * text.split(" ").length} minutes</p>
+
+            </div>
+        </>
+    )
+}
